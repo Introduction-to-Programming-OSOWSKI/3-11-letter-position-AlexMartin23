9115997c16f38f9e071036c1cf7bea912e1d01c2
AlexMartin23 <832007@mystma.org>
2021-02-04 18:29:52 +0000

    letter position try 1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(strWord, strLetter):
+    srtTest = "" #initialize test variable 
+    intreturn = 0
+    for i in range (0, len(strWord)-1):
+        strTest = strWord[i]
+        if strTest == strLetter:
+            return i + 1
+    return 0
+
+
+print(letterPos("alex", "q"))  
+        
+    
